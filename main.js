a49e9b198823f6e0b68d0475e71bcb27a816513b
skiednia <agatha.floors@gmail.com>
2019-09-02 11:10:55 -0300

    finalizando os comentarios

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ function convertTemp(direction)
 var fObj = document.convert.ftemp, cObj = document.convert.ctemp, kObj = document.convert.ktemp;
 
 
-if (direction == "ftoc") 
+if (direction == "ftoc") // if para troca de f para de F para C //
 {
     cObj.value = Math.round((fObj.value - 32) * (5/9));
 	kObj.value = Math.round((parseInt(cObj.value) + 459.67) * (5/9));
 } 
-else if (direction == "ktof") 
+else if (direction == "ktof") // F para K //
  {
 	fObj.value = Math.round((parseInt(cObj.value) * (9/5)) -  459.67);
 	cObj.value = Math.round((fObj.value - 32) * (5/9));
 	
  }
-else
+else // C para K e F //
  {
 	fObj.value = Math.round((parseInt(cObj.value) * (9/5)) + 32);
 	kObj.value = Math.round((parseInt(cObj.value) + 273));
  }
 }
 
-function clearAll()
+function clearAll() // referente ao botão para limpar //
 {
 document.convert.ftemp.value="";
 document.convert.ctemp.value="";
